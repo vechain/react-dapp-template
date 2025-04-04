383538d8af9cefbf0d522582625123bbb1adf51d
Frank <andreass.frankk@gmail.com>
2025-04-04 14:17:53 +0300

    added the basics

--- apps/frontend/src/hooks/useFiorinoMinter.ts
+++ apps/frontend/src/hooks/useFiorinoMinter.ts
@@ -1,20 +1,19 @@
-import { getConfig } from "@repo/config";
-import { Fiorino__factory } from "@repo/contracts";
-import { getCallKey, useCall } from "../utils/hooks/useCall";
-import { useWallet } from "@vechain/vechain-kit";
-import { compareAddresses } from "@repo/utils/AddressUtils";
+import {getConfig} from "@repo/config";
+import {Fiorino__factory} from "@repo/contracts";
+import {getCallKey, useCall} from "../utils/hooks/useCall";
+import {useWallet} from "@vechain/vechain-kit";
+import {compareAddresses} from "@repo/utils/AddressUtils";
 
-const contractAddress = '0x31454bc37feCC3855bf9DF1cA769f25C82eD1e98'
-
+const contractAddress = getConfig(import.meta.env.VITE_APP_ENV).fiorinoContractAddress;
 const contractInterface = Fiorino__factory.createInterface();
 const method = "owner";
 
 export const getFiorinoMinterQueryKey = () => {
-  getCallKey({ method });
+  getCallKey({method});
 };
 
 export const useFiorinoMinter = () => {
-  const { account } = useWallet();
+  const {account} = useWallet();
   const results = useCall({
     contractInterface,
     contractAddress,
